02a665318ba267882b52bb334a79244102f9e7d0
Reena rani <reenayadav560@gmail.com>
2018-05-28 16:02:25 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/reena/twittermap/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.reena.twittermap;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by AbhiAndroid
+ */
+
+public class SplashScreen extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
+
